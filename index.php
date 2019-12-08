ae128501cd1fc9ef56697e33b15280aa83f8be56
Saksham Kushwaha <13682225+aistore2030@users.noreply.github.com>
2019-12-08 13:37:39 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+header("refresh: 5; https://xpagg.com/public/");
+
+	echo '<title>Laravel Installed</title><div style="background: #e9ffed; border: 1px solid #b0dab7; padding: 15px;" align="center" >
+	<font size="5" color="#182e7a">Laravel is installed successfully.</font><br /><br />
+	<font size="4">Laravel is a Framework and doesn\'t have an index page.<br /><br />
+	You will be redirected to its "public" folder in 5 seconds...<br /><br />
+	Laravel is a clean and classy framework for PHP web development.
+
+Freeing you from spaghetti code, Laravel helps you create wonderful applications using simple, expressive syntax. Development should be a creative experience that you enjoy, not something that is painful. Enjoy the fresh air.
+</font></div>';
+?>
